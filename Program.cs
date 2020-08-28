b5249ecd2d254a05529808e407dd35ac0ca44b81
bloknet <barinov_tosha@inbox.ru>
2020-08-28 10:18:12 +0300

    mimax

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace CWminMax
+{
+    class Program
+    {
+        public static int[] minMax(int[] lst){
+            int[] arr = new int[2];
+            Array.Sort(lst);
+            int min = lst[0];
+            //Console.WriteLine(min);
+            Array.Reverse(lst);
+            int max = lst[0];
+            //Console.WriteLine(max);
+            arr[0] = min;
+            arr[1] = max;
+           
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+            int[] thearr = {4,7,1,9,5};
+           minMax(thearr);
+        }
+    }
+}
